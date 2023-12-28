5d4a0e344c4d7eb567292042974cd4661bb1c0a7
brecht <brecht.baeten@enervalis.com>
2023-12-28 19:25:28 +0100

    added gameId share

--- src/components/GameId.js
+++ src/components/GameId.js
@@ -1,26 +1,41 @@
+import React, { useState } from 'react';
+import Snackbar from '@mui/material/Snackbar';
+
 export default function GameId(props){
   const gameId = props.gameId;
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
 
   const shareGameId = () => {
     let value = gameId;
     if (window.location) {
       value = window.location.href.replace("/hide", "/seek")
     }
-
+    console.log(navigator)
     if(navigator.share) {
-      navigator.share(value);
+      navigator.share({url: value});
     }
     else if(navigator.clipboard) {
+      setSnackbarMessage("Copied to clipboard");
+      setSnackbarOpen(true);
       navigator.clipboard.writeText(value);
     }
-    else(
-      console.log("not supported")
-    )
+    else {
+      setSnackbarMessage("Not supported on your device");
+      setSnackbarOpen(true);
+    }
   };
 
+  const handleClose = () => {
+    setSnackbarOpen(false);
+  }
+
   return (
-    <button style={{color: "#1976d2", cursor: "pointer", border: "none", background: "none", padding: 0, font: "inherit"}} onClick={() => shareGameId()}>
-      Game ID: {gameId}
-    </button>
+    <div>
+      <button style={{color: "#1976d2", cursor: "pointer", border: "none", background: "none", padding: 0, font: "inherit"}} onClick={() => shareGameId()}>
+        Game ID: {gameId}
+      </button>
+      <Snackbar open={snackbarOpen} autoHideDuration={1000} onClose={handleClose} message={snackbarMessage} />
+    </div>
   )
 }
